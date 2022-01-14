fc09851ebc2268c034e87d375b44ed718867b26f
Katz Ueno <katz@macareux.co.jp>
2022-01-14 22:29:46 +0900

    FormBlockSubmissionEmailNotification: initial placement of notify()

---
+++ application/src/Express/Entry/Notifier/Notification/FormBlockSubmissionEmailNotification.php
@@ -0,0 +1,49 @@
+<?php
+namespace Concrete\Core\Express\Entry\Notifier\Notification;
+
+use Concrete\Core\Express\Entry\Notifier\Notification\FormBlockSubmissionEmailNotification as CoreFormBlockSubmissionEmailNotification;
+use Concrete\Core\Entity\Attribute\Value\ExpressValue;
+use Concrete\Core\Entity\Express\Entry;
+use Concrete\Core\User\UserInfoRepository;
+use Doctrine\ORM\EntityManager;
+
+class FormBlockSubmissionEmailNotification extends CoreFormBlockSubmissionEmailNotification
+{
+   public function notify(Entry $entry, $updateType)
+    {
+        $files = [];
+        if ($this->blockController->notifyMeOnSubmission) {
+            $mh = $this->app->make('mail');
+            $mh->to($this->getToEmail());
+            $mh->from($this->getFromEmail($entry));
+            $mh->replyto($this->getReplyToEmail($entry));
+            $mh->addParameter('entity', $entry->getEntity());
+            $mh->addParameter('formName', $this->getFormName($entry));
+            $mh->addParameter("dataSaveEnabled", $this->blockController->storeFormSubmission);
+            if (!$this->blockController->storeFormSubmission) {
+                //if save submitted data is not active we send also files as attachments in email becuase it will be removed after entry remove
+                foreach ($this->getAttributeValues($entry) as $attributeValue) {
+                    if ($attributeValue->getAttributeTypeObject()->getAttributeTypeHandle() == "image_file") {
+                        $file = $attributeValue->getValue();
+                        if ($file) {
+                            $files[] = $file;
+                            $mh->addAttachment($file);
+                        }
+                    }
+                }
+            }
+            $mh->addParameter('attributes', $this->getAttributeValues($entry));
+            $mh->load('block_express_form_submission');
+            if (empty($mh->getSubject())) {
+                $mh->setSubject(t('Website Form Submission – %s', $this->getFormName($entry)));
+            }
+            $mh->sendMail();
+            //we have to delete the files as they are created automatically and are not attached to the entry save
+            if (!$this->blockController->storeFormSubmission) {
+                foreach ($files as $file) {
+                    $file->delete();
+                }
+            }
+        }
+    }
+}
