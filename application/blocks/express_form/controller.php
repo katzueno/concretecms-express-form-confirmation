45a96f0ad137a71a03421116d99675cd8436c86d
Katz Ueno <katz@macareux.co.jp>
2022-01-14 22:27:41 +0900

    Express Form Contoller override

---
+++ application/blocks/express_form/controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Concrete\Block\ExpressForm;
+
+use Concrete\Block\ExpressForm\Conttoller as CoreController;
+use Application\Core\Express\Entry\Notifier\Notification\FormBlockSubmissionEmailNotification;
+use Concrete\Core\Express\Entry\Notifier\Notification\FormBlockSubmissionNotification;
+
+class Controller extends CoreController
+{
+    public function getNotifications()
+    {
+        $notifications = [new FormBlockSubmissionEmailNotification($this->app, $this)];
+        //if we don't save data we must not use this notifier because entry is already not saved
+        if ($this->areFormSubmissionsStored()) {
+            array_unshift($notifications, new FormBlockSubmissionNotification($this->app, $this));
+        }
+
+        return $notifications;
+    }
+}
